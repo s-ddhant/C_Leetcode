fc373aa45adba390d4f8b79cd1ac0a440bd363fe
s-ddhant <149889288+s-ddhant@users.noreply.github.com>
2024-08-29 22:19:26 +0530

    Time: 121 ms (40.68%) | Memory: 8.5 MB (46.41%) - LeetSync

---
+++ 1-two-sum/two-sum.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){    
+    int *temp = malloc(2 *sizeof(int));
+    temp[0] = 0;
+    temp[1] = 1;
+    for(int i = 0;i < numsSize ;i++)
+    {
+        for(int j = i + 1; j < numsSize; j++)
+        {
+            if(nums[i] + nums[j] == target)
+            {
+                *returnSize = 2;
+                temp[0] = i;
+                temp[1] = j;
+                return temp;
+
+            }
+        }
+    }
+    *returnSize = 0;
+    return 0;
+    free(temp);
+}
